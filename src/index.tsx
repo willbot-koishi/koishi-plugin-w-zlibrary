52a74caa0d42a1086695130157b92e5e611ac8e0
ForkΨKILLET <fork_killet@outlook.com>
2024-11-05 02:37:30 +0800

    feat: `zlib.detail`

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import { capitalize, Context, z } from 'koishi'
+import { capitalize, Context, SessionError, z } from 'koishi'
 import {} from 'koishi-plugin-w-assets-remote'
 import {} from 'koishi-plugin-w-as-forward'
 import {} from 'koishi-plugin-w-as-slices'
@@ -7,7 +7,7 @@ import * as cheerio from 'cheerio'
 
 export const name = 'w-zlibrary'
 
-export const inject = [ 'http', 'assetsAlt' ]
+export const inject = [ 'http', 'assetsAlt', 'database' ]
 
 export interface Config {
     cookie: string
@@ -19,10 +19,27 @@ export const Config: z<Config> = z.object({
     domain: z.string().default('z-lib.fm').description('要使用的 zlibrary 域名')
 })
 
+const escapeRegExp = (segments: TemplateStringsArray, ...args: string[]) => {
+    const re = segments
+        .map((segment, index) => segment + (args[index]?.replace(/[-[\]{}()*+?.,\\^$|#]/g, '\\$&') ?? ''))
+        .join('')
+    return new RegExp(re, 'g')
+}
+
 export function apply(ctx: Context, config: Config) {
     ctx.command('zlib', 'zlibrary 功能')
 
     const getLoginStat = (username?: string) => username ? `已登录：${username}` : '未登录'
+
+    const validateDetailUrl = (url: string) => {
+        const match = url.match(escapeRegExp`^((https?://)?${config.domain})?/book/.*\\.html$`)
+        if (! match) throw new SessionError('id', [ <>请提供正确的 zlibrary <strong>详情</strong>链接</> ])
+        const path = url.replace(escapeRegExp`^(https?://)?${config.domain}`, '')
+        return {
+            path,
+            url: `https://${config.domain}${path}`
+        }
+    }
 
     ctx.command('zlib.loginstat', '查看 zlibrary 登录状态')
         .action(async () => {
@@ -37,6 +54,46 @@ export function apply(ctx: Context, config: Config) {
             const username = $('.user-card__name').text().trim()
             return getLoginStat(username)
         })
+
+    ctx.i18n.define('en-US', {
+        id: '{0}'
+    })
+
+    interface Book {
+        title: string
+        authors: string[]
+        coverUrl?: string
+        url: string
+        downloadUrl?: string
+        year: number
+        language: string
+        fileSize?: string
+        extension?: string
+        rating: number
+        quality: number
+    }
+
+    const getUrl = (url: string, short: boolean) => (short ? '' : `https://${config.domain}`) + decodeURI(url) 
+
+    const renderRating = (rating: number) => '★'.repeat(rating) || '☆'
+
+    const renderBook = (options: { shortUrl: boolean, header?: any }) => (book: Book, index?: number) => <>
+        { options.header ?? <></> }
+        { typeof index === 'number' ? <>
+            <br /> [#{index + 1}]
+        </> : '' }
+        <br /> [标题] { book.title }
+        { book.coverUrl ? <>
+            <br /> [封面] <img src={book.coverUrl} />
+        </> : '' }
+        <br /> [作者] { (book.authors.length > 2 ? book.authors.slice(0, 2).concat('...') : book.authors).join('; ') }
+        <br /> [年份] { book.year || 'N/A' } [语言] { capitalize(book.language) }
+        <br /> [评分] { renderRating(book.rating) } [质量] { renderRating(book.quality) }
+        <br /> [详情] <a href={ getUrl(book.url, options.shortUrl) }></a>
+        { book.downloadUrl ? <>
+            <br /> [大小] { book.fileSize } [类型] { book.extension } [下载] <a href={ getUrl(book.downloadUrl, options.shortUrl) }></a>
+        </> : '' }
+    </>
 
     ctx.command('zlib.search <filter:text>', '在 zlibrary 中搜索书籍')
         .option('shortUrl', '-s 显示短链接')
@@ -60,7 +117,7 @@ export function apply(ctx: Context, config: Config) {
             const $ = cheerio.load(html)
             const items = $('z-bookcard')
                 .toArray()
-                .map(el => {
+                .map((el): Book => {
                     const $item = $(el)
                     const $title = $item.find('[slot=title]')
                     const $author = $item.find('[slot=author]')
@@ -71,27 +128,16 @@ export function apply(ctx: Context, config: Config) {
                         downloadUrl: $item.attr('download'),
                         year: + $item.attr('year'),
                         language: $item.attr('language'),
-                        size: $item.attr('filesize'),
-                        rating: + $item.attr('rating')
+                        fileSize: $item.attr('filesize'),
+                        extension: $item.attr('extension'),
+                        rating: + $item.attr('rating'),
+                        quality: + $item.attr('quality')
                     }
                 })
 
             const username = $('.user-card__name').text().trim()
 
-            const getUrl = (url: string) => (shortUrl ? '' : `https://${config.domain}`) + decodeURI(url) 
-
-            const itemTexts = items
-                .map((item, index) => <>
-                    <br />
-                    <br /> [#{index + 1}]
-                    <br /> [标题] { item.title }
-                    <br /> [作者] { (item.authors.length > 2 ? item.authors.slice(0, 2).concat('...') : item.authors).join('; ') }
-                    <br /> [年份] { item.year || 'N/A' } [语言] { capitalize(item.language) } [评分] { '★'.repeat(item.rating) || '☆' }
-                    <br /> [详情] <a href={getUrl(item.url)}></a>
-                    { item.downloadUrl && <>
-                        <br /> [大小] { item.size } [下载] <a href= {getUrl(item.downloadUrl) }></a>
-                    </> }
-                </>)
+            const itemTexts = items.map(renderBook({ shortUrl, header: <br /> }))
 
             const pageTotal = + $('.paginator + script').text().match(/pagesTotal:\s*(\d+)/)?.[1] || '?'
             const pageText = `（第 ${page ?? 1}/${pageTotal} 页）`
@@ -106,23 +152,51 @@ export function apply(ctx: Context, config: Config) {
             </as-forward>
         })
 
-    ctx.command('zlib.store <url:string>', '转存 zlibrary 的书籍到 Koishi', { authority: 2 })
+    ctx.command('zlib.detail <url:string>', '查看 zlibrary 书籍详情')
         .action(async ({ session }, url) => {
-            const escapeRegExp = (segments: TemplateStringsArray, ...args: string[]) => {
-                const re = segments
-                    .map((segment, index) => segment + (args[index]?.replace(/[-[\]{}()*+?.,\\^$|#]/g, '\\$&') ?? ''))
-                    .join('')
-                return new RegExp(re, 'g')
-            }
-
-            const match = url.match(escapeRegExp`(https?://|/)?${config.domain}/book/.*\\.html`)
-            if (! match) return <>请提供正确的 zlibrary <strong>详情</strong>链接</>
-            const path = new URL(url).pathname
-            url = `https://${config.domain}${path}`
+            const { url: detailUrl } = validateDetailUrl(url)
+            const startTime = Date.now()
 
             session.send(<>正在请求详情页面……</>)
+            const html = await ctx.http.get(detailUrl, {
+                responseType: 'text',
+                headers: {
+                    Cookie: config.cookie
+                }
+            })
+            const $ = cheerio.load(html)
 
-            const html = await ctx.http.get(url, {
+            const [ fileType, fileSize ] = $('.property__file > .property_value').text().split(', ')
+
+            const book: Book = {
+                title: $('.book-title').text(),
+                coverUrl: $('.details-book-cover-container > z-cover img').data('src') as string,
+                authors: $('.book-title + i > a').toArray().map(el => $(el).text()),
+                url: detailUrl,
+                downloadUrl: $('a[href^="/dl/"]').attr('href'),
+                year: + $('.property_year > .property_value').text(),
+                language: $('.property_language > .property_value').text(),
+                fileSize,
+                extension: fileType,
+                rating: + $('.book-rating-interest-score').text(),
+                quality: + $('.book-rating-quality-score').text()
+            }
+            
+            const endTime = Date.now()
+            const durationText = ((endTime - startTime) / 1000).toFixed(2)
+
+            return <as-forward level='always'>
+                已获取书籍详情，用时 {durationText} 秒<br />
+                { renderBook({ shortUrl: false })(book) }
+            </as-forward>
+        })
+
+    ctx.command('zlib.store <url:string>', '转存 zlibrary 的书籍到 Koishi', { authority: 2 })
+        .action(async ({ session }, url) => {
+            const { url: detailUrl, path: detailPath } = validateDetailUrl(url)
+
+            session.send(<>正在请求详情页面……</>)
+            const html = await ctx.http.get(detailUrl, {
                 responseType: 'text',
                 headers: {
                     Cookie: config.cookie
@@ -142,7 +216,7 @@ export function apply(ctx: Context, config: Config) {
             })
 
             const extname = $('.book-property__extension').text()
-            const filename = path.slice('/book/'.length).replaceAll('/', '_').replace(/html$/, extname)
+            const filename = detailPath.slice('/book/'.length).replaceAll('/', '_').replace(/html$/, extname)
             const assetUrl = await ctx.assetsAlt.uploadFile(downloadBlob, filename)
 
             return <>成功转存到 Koishi：{assetUrl}</>
